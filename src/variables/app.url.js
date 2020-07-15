6675506202752d0c05284b4aee41f89caadb2331
kumar abhishek <mail2kumar.2021@gmail.com>
2020-07-15 16:43:27 +0400

    3.17.204.184

--- src/variables/app.url.js
+++ src/variables/app.url.js
@@ -5,7 +5,7 @@ const servicePort = 8080;
 const app = {
     uiEndPoint: `http://18.223.24.198:${uiPort}/`
     // uiEndPoint: `http://localhost:${uiPort}/`
-    , serviceEndPoint: `http://52.14.205.131:${servicePort}/`
+    , serviceEndPoint: `http://3.17.204.184:${servicePort}/`
     //, serviceEndPoint: `http://localhost:${servicePort}/`
 }
 
